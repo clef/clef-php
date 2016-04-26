5f8f1277d21e3dcd4b43969492b7dd6f32ec4c61
Jesse Pollak <jesse@pollak.io>
2016-04-26 15:44:18 -0700

    fix(urlsafe_base64encode): encodes = correctly

--- lib/Encoding.php
+++ lib/Encoding.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 
 namespace Clef;
 
 trait Encoding {
-    public function base64url_encode($data) { 
-        return strtr(base64_encode($data), '+/', '-_'); 
-    } 
+    public function base64url_encode($data) {
+        return strtr(base64_encode($data), '+/=', '-_,');
+    }
 
-    public function base64url_decode($data) { 
-        return base64_decode(strtr($data, '-_', '+/')); 
-    } 
+    public function base64url_decode($data) {
+        return base64_decode(strtr($data, '-_=', '+/,'));
+    }
 }
